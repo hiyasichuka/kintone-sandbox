b13bd0c53211e6574053884ba5f90a6533d60942
hiyasichuka <hiyasichukahajimemasita@gmail.com>
2024-04-15 20:53:45 +0900

    Add process.env.APP

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 import { KintoneRestAPIClient } from '@kintone/rest-api-client'
+import { exit } from 'process'
 
 require('dotenv').config()
+
+if (!process.env.APP) exit()
 
 const client = new KintoneRestAPIClient({
   baseUrl: process.env.URL,
@@ -13,12 +16,10 @@ const client = new KintoneRestAPIClient({
   auth: { apiToken: process.env.KINTONE_API_TOKEN },
   // Use OAuth token authentication
   // auth: { oAuthToken: process.env.KINTONE_OAUTH_TOKEN }
-
-  // Use session authentication if `auth` is omitted (in browser only)
 })
 
 client.record
-  .getRecords({ app: '9' })
+  .getRecords({ app: process.env.APP })
   .then((resp) => {
     console.log(resp.records)
   })
